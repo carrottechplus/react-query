6636d924dcfb1cf31e4542d855a335fbcaa20d64
carrottechplus <blohzzang@gmail.com>
2023-07-27 11:45:14 +0900

    데이터 캐싱, stale 시간 설정

--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -7,7 +7,16 @@ function UserInfo() {
 		return await response.json();
 	};
 
-	const { data, isLoading, isSuccess, isError } = useQuery(['user', 2], fetchUser);
+	const { data, isLoading, isSuccess, isError } = useQuery(['user', 2], fetchUser, {
+		// 기본값은 true이나 false로 설정해주는게 좋다
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+		//
+
+		staleTime: 1000 * 5, // default : 0ms, 처음에 fresh 였다가 5초 후 refetch 되어 stale 상태로
+		cacheTime: 1000 * 5, // 캐시에 데이터 유지시키는 시간, 위와 세트로 다님, default: 5m, 해당 시간 뒤에 가비지컬렉터에 의해 메모리에거 값 제거.
+	});
+
 	console.log(data);
 	console.log('isLoading', isLoading);
 	console.log('isSuccess', isSuccess);
