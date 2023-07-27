7217ad22873a4dbdec80e3803a045e4d6e3337fc
carrottechplus <blohzzang@gmail.com>
2023-07-27 11:14:04 +0900

    react-query 설치 및 devtool세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import UserInfo from './UserInfo';
 
 function App() {
-	return <div className='App'>Hello</div>;
+	const queryClient = new QueryClient();
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div className='App'>
+				<h1>Hello</h1>
+				<UserInfo />
+			</div>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
